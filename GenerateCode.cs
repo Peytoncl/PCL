f6ebd1a567670948dff6de30bb4bb7d8ea9af08f
Peytoncl <peytonthegreat0@gmail.com>
2022-02-12 00:18:03 -0500

    init

---
+++ GenerateCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace PCL
+{
+    class GenerateCode
+    {
+        static string template = "public class template : MonoBehaviour\n{\nhere\n}";
+
+        static void Main(string[] args)
+        {
+            string filePath = args[0];
+
+            if(filePath == "update")
+            {
+                
+            }
+
+            string fileContents = File.ReadAllText(filePath);
+
+            string newScript = template;
+
+            newScript = newScript.Replace("template", Path.GetFileNameWithoutExtension(filePath));
+            newScript = newScript.Replace("here", MakeCs.Generate(fileContents));
+
+            List<string> allUsings = new List<string>();
+            foreach(string line in newScript.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if(line.Contains("using"))
+                {
+                    foreach(string potentialUsing in line.Split("\n"))
+                    {
+                        if(potentialUsing.Contains("using"))
+                        {
+                            newScript = newScript.Replace(potentialUsing, "");
+
+                            allUsings.Add(potentialUsing + "\n");
+                        }
+                    }
+                }
+            }
+
+            if(File.Exists(filePath + ".cs"))
+            {
+                File.Delete(filePath + "cs");
+            }
+            File.Create(filePath + ".cs").Close();
+
+            foreach(string using1 in allUsings)
+            {
+                var sb = new StringBuilder();
+                sb.Append(newScript);
+                sb.Insert(0, using1);
+                newScript = sb.ToString();
+            }
+
+            File.WriteAllText(filePath + ".cs", newScript);
+        }
+    }
+}
---
+++ MakeCs.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace PCL
+{
+    public class MakeCs
+    {
+        static string[] tokens1 = {"local var", "fun void"};
+        static string[] statements = {"if", "while", "for", "foreach", "do"};
+
+        public static string Generate(string script)
+        {
+            string newScript = script;
+
+            foreach(string token in tokens1)
+            {
+                try
+                {
+                    newScript = newScript.Replace(token.Split(" ")[0], token.Split(" ")[1]);
+                }
+                catch {}
+            }
+
+            foreach(string line in script.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach(string statement in statements)
+                {
+                    if(line.Contains(statement))
+                    {
+                        string newLine = line.Replace(statement, statement + "(");
+                        newLine += ")";
+
+                        newScript = newScript.Replace(line, newLine);
+                    }
+                }
+            }
+
+            return newScript;
+        }
+    }
+}
